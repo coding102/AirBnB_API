0cc4d21eb45276d18e06619a8c3630f3ce8a1a9d
Marck <mercercountynj@gmail.com>
2022-07-14 16:42:28 -0400

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+     namespace :api, dafaults: {format: :json} do
+          namespace :v1 do
+               get '/logout' => 'users#logout'
+               post '/fabook' => 'users#facebook'
+          end
+     end
+
 end
